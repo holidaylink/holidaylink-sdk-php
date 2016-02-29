83712b4a2b47d35de0e9807d09989eb218021263
Toni Butkovic <tonibutkovic23@gmail.com>
2016-02-29 16:21:23 +0100

    sdk improvements

--- lib/HolidayLink/Api/AccommodationFeatureValue.php
+++ lib/HolidayLink/Api/AccommodationFeatureValue.php
@@ -2,6 +2,10 @@
 
 namespace HolidayLink\Api;
 
+use HolidayLink\Auth\Credentials;
+use HolidayLink\Transport\JsonCall;
+use HolidayLink\Transport\XmlCall;
+
 /**
  * Class AccommodationFeatureValue
  * @package HolidayLink\Api
@@ -17,5 +21,68 @@ class AccommodationFeatureValue extends Model {
     'parent_id',
     'parent',
   ];
+
+  /**
+   * Retrieve single accommodation feature value matching the $code filter
+   *
+   * @param  string $code
+   * @param  array $params
+   * @param  Credentials $credentials API credentials
+   *
+   * @return self  the retrieved accommodation feature value
+   */
+  public static function singleFromXML ($code, array $params = [], Credentials $credentials = null) {
+    if (!empty($credentials)) {
+      self::setCredentials($credentials);
+    }
+
+    $allowedParams = array(
+      'expand' => 1,
+    );
+
+    $wrongParams = array_diff_key($params, $allowedParams);
+    if (!empty($wrongParams)) {
+      throw new \InvalidArgumentException('Invalid $params filter: ' . implode(', ', array_keys($wrongParams)));
+    }
+
+    $call = new XmlCall($credentials);
+    $sxe = $call->execute('accommodation-feature-values/' . $code, 'GET', array_intersect_key($params, $allowedParams));
+
+    $ret = new self();
+    $ret->fromXML($sxe);
+
+    return $ret;
+  }
+
+  /**
+   * Update single accommodation feature value matching the $code filter and array of key => value params
+   *
+   * @param  string $code
+   * @param  array $params
+   * @param  array $data
+   * @param  Credentials $credentials API credentials
+   *
+   * @return Accommodation feature value
+   */
+  public static function updateSingle ($code, array $params = [], array $data= [], Credentials $credentials = null) {
+    if (!empty($credentials)) {
+      self::setCredentials($credentials);
+    }
+
+    $allowedParams = array(
+      'expand' => 1,
+    );
+
+    $wrongParams = array_diff_key($params, $allowedParams);
+    if (!empty($wrongParams)) {
+      throw new \InvalidArgumentException('Invalid $params filter: ' . implode(', ', array_keys($wrongParams)));
+    }
+
+    $call = new JsonCall($credentials);
+    $sxe = $call->execute('accommodation-feature-values/' . $code, 'PUT', array_intersect_key($params, $allowedParams),
+      $data);
+
+    return $sxe;
+  }
 
 }
